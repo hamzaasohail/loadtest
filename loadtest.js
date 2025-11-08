89880951e06c81e25eb0f051b3f94e170b4e32b9
malik-hamza29 <hmalik633@gmail.com>
2025-11-08 17:08:19 +0500

    increase target for final load stage to enhance performance testing

--- loadtest.js
+++ loadtest.js
@@ -6,7 +6,7 @@ export const options = {
         { duration: '1m', target: 50 },
         { duration: '3m', target: 200 },
         { duration: '20s', target: 150 },
-        { duration: '10s', target: 100 },
+        { duration: '10s', target: 300 },
     ],
 }
 
