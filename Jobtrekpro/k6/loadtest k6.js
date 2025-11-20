a726eb8352730fdbe9be4b6e68e951d94d17d5c9
malik-hamza29 <hmalik633@gmail.com>
2025-11-20 16:24:14 +0500

    s

--- Jobtrekpro/k6/loadtest k6.js
+++ Jobtrekpro/k6/loadtest k6.js
@@ -6,7 +6,7 @@ export const options = {
         { duration: '1m', target: 100 },
         { duration: '1m', target: 150 },
         { duration: '1m', target: 300 },
-        { duration: '1m', target: 200 },
+        { duration: '1m', target: 300 },
     ],
 }
 
